6a58ea4572595218e581d7a2e37a04a9f6c1b144
τaomind <151855768+taomind007@users.noreply.github.com>
2024-02-15 22:10:04 -0800

    update comments

--- main.py
+++ main.py
@@ -1,9 +1,3 @@
-
-wallet = "your wallet"
-hotkeys = ["your hotkeys"] #a list with the names of all the hotkeys you want to register
-highest_cost = 2.0 #The maximal amount of Tao you are willing to burn to register
-password = "your password" #Password for your cold key
-
 import pexpect
 import re
 import sys
@@ -11,38 +5,39 @@ import time
 import traceback
 from datetime import datetime
 
-iterate=False
+# configure options for your command
+wallet = "your wallet name"       # wallet name of your coldkey
+hotkeys = ["your hotkeys"]        # a list with the names of all the hotkeys you want to register
+highest_cost = 2.0                # the maximal amount of Tao you are willing to burn to register
+password = "your password"        # password for your coldkey
+netuid = 1                        # subnet uid you want to register
+iterate = False
+
+# start registraion bot
 while True:
     for hotkey in hotkeys:
         while True:
             try:
-                iterate=False
-                command = 'btcli subnet register --subtensor.network local --netuid 21 --wallet.name {} --wallet.hotkey {}'.format(wallet,hotkey)
-                # Get the current time
-                current_time = datetime.now().time()
+                iterate = False
+                command = 'btcli subnet register --subtensor.network local --netuid {} --wallet.name {} --wallet.hotkey {}'.format(netuid, wallet,hotkey)
                 
                 # Format the time as HH:MM:SS
-                formatted_time = current_time.strftime("%H:%M:%S")
+                formatted_time = datetime.now().time().strftime("%H:%M:%S")
                 
                 # Print the formatted time
-                print("\nColdkey:",wallet,"Hotkey:", hotkey, flush=True)
-                print(formatted_time,flush=True)
+                print("\nColdkey: ", wallet, "Hotkey: ", hotkey, flush=True)
+                print(formatted_time, flush=True)
 
                 child = pexpect.spawn(command)
-                
                 child.logfile_read = sys.stdout.buffer
-                
-                # child.expect('Enter subtensor network')
-                # print("\nSending: <enter>", flush=True)
-                # child.sendline('')
-                
-                child.expect(r'The cost to register by recycle is (.*?)(?:\n|$)')
+                                
+                child.expect(r'Registraion cost is (.*?)(?:\n|$)')
                 cost_str = child.match.group(1).decode('utf-8').replace('τ', '')
                 clean_cost_str = re.sub(r'\x1b\[[0-9;]*m', '', cost_str).strip()
                 cost = float(clean_cost_str)
                 
                 if cost > highest_cost:
-                    print("Not buying: n", flush=True)
+                    print("Not registering: n", flush=True)
                     child.sendline('n')
                     continue
                 else:
@@ -57,6 +52,7 @@ while True:
                     child.expect(r'Recycle (.*?) to register on subnet')
                 except:
                     break
+                    
                 recycle_cost_str = child.match.group(1).decode('utf-8').replace('τ', '')
                 clean_recycle_cost_str = re.sub(r'\x1b\[[0-9;]*m', '', recycle_cost_str).strip()
                 recycle_cost = float(clean_recycle_cost_str)
@@ -69,7 +65,7 @@ while True:
                     print("Sending2: y", flush=True)
                     child.sendline('y')
                     
-                    child.expect(r'Registered',timeout=120)
+                    child.expect(r'Registered', timeout=120)
                     break
             except Exception as e:
                 print("An error occured", e)
@@ -80,3 +76,4 @@ while True:
                     break
                 else:
                     continue
+                    
